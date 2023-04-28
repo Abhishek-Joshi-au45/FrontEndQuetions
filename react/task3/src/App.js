c33beeeb10eebab4eb1eb61ff094c997302781cf
Abhishek joshi <joshiabhishek2030@gmail.com>
2023-04-28 10:19:35 +0530

    task 3

---
+++ react/task3/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ApiComponent from './ApiComponent';
+
+function App() {
+  return (
+    <div>
+      <ApiComponent />
+    </div>
+  );
+}
+
+export default App;
